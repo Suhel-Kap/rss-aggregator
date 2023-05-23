296980d54663117d58dc6dddc2fbd25c596ec92c
Suhel Kapadia <suhelkapadia2@gmail.com>
2023-05-23 23:32:37 +0530

    init: initial setup

---
+++ README.md
@@ -0,0 +1,25 @@
+# RSS Aggregator
+
+This project is an RSS aggregator that fetches and displays RSS feeds from various sources.
+
+## Installation
+
+To install the project, simply clone the repository and run the following command:
+
+```
+go mod download
+```
+
+## Usage
+
+To run the project, simply run the following command:
+
+```
+go run main.go
+```
+
+This will start the server and you can access the aggregator at http://localhost:8000.
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found in config")
+	}
+
+	log.Printf("Server running on port %s\n", portString)
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
